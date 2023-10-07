6124cab34900550a1de66e1f6cffb0beed2fec0d
sofiapetushok <sofiapetushok@outlook.com>
2023-10-07 21:45:38 +0300

    code clean, add reset, color input

--- js/main.js
+++ js/main.js
@@ -1,50 +1,107 @@
+// Grid
 const container = document.querySelector('#container');
 console.log(container);
- 
 const rows = document.querySelector('.gridRow');
 const cols = document.querySelector('.gridCol');
+let cell = document.querySelector('.grid-item');
+const setGridBtn = document.querySelector('#btn-alert');
+const gridBtn = document.querySelector('#gridRange');
+const applyBtn = document.querySelector('#applyBtn');
+// Buttons
 const clearBtn = document.querySelector('#clear-cells');
+const randColorBtn = document.querySelector('#randomBtn');
+const colorBtn = document.querySelector('#colorBtn');
 
 
-const setBg = (el) => {
-    const randColor = Math.floor(Math.random()*16777215).toString(16);   
-    el.style.backgroundColor = "#" + randColor;
+const getRandomColor = () => {
+    const randColor = Math.floor(Math.random() * 16777215).toString(16);
+    return "#" + randColor;
+};
+const colorBg = (el) => {
+    let inputColor = document.querySelector('#colorPicker').value;
+    console.log(inputColor);
+    el.style.backgroundColor = inputColor;
+
 }
-const darkenBg = (el) => {
-    const color = 
+const setRandBg = (el) => {
+    const randColorHex = getRandomColor();   
+    el.style.backgroundColor = randColorHex;
 }
-
+const eraseBg = (el) => {
+    el.style.backgroundColor = '#fff';
+}
+let currentMode = 'color'; 
+let isMouseDown = false;
 function makeRows(rows, cols) {
+    container.innerHTML = '';
     container.style.setProperty('--grid-rows', rows);
-    console.log(rows);
     container.style.setProperty('--grid-cols', cols);
-    console.log(cols);
+
     for ( c = 0; c < (rows*cols); c++) {
         let cell = document.createElement('div');
-        // cell.textContent = '';
         cell.classList.add('grid-item');
-        cell.addEventListener('mouseenter', () => {
-            // setBg(cell);
-            
-        })
         container.appendChild(cell);
+
+        cell.addEventListener('mousedown', () => {
+            isMouseDown = true; // Mouse button is held down
+            handleCellAction(cell); // Perform initial action
+        });
+
+        cell.addEventListener('mouseenter', () => {
+            if (isMouseDown) {
+                handleCellAction(cell); // Perform action while hovering
+            }
+        });
+
+        cell.addEventListener('mouseup', () => {
+            isMouseDown = false; // Mouse button is released
+        });
+
         clearBtn.addEventListener('click', () => {
             cell.style.backgroundColor = '#fff';
-        })
+        });
 
     }
 }
+makeRows(20,20);
 
-makeRows(16,16);
-let cell = document.querySelector('.grid-item');
-let button = document.querySelector('#btn-alert');
+// Event listener for colorBtn
+colorBtn.addEventListener('click', () => {
+    currentMode = 'color';
+});
 
-button.addEventListener('click', () => {
-    let squares = prompt('Enter rows and columns');
-    if (squares < 100) {
-        makeRows(squares,squares);
-    } else {
-        alert("Too many");
+// Event listener for randColorBtn
+randColorBtn.addEventListener('click', () => {
+    currentMode = 'randColor';
+});
+
+// Event listener for eraseBtn
+eraseBtn.addEventListener('click', () => {
+    currentMode = 'erase';
+});
+
+
+function handleCellAction(cell) {
+    // Apply the corresponding action based on the current mode
+    if (currentMode === 'color') {
+        colorBg(cell);
+    } else if (currentMode === 'randColor') {
+        setRandBg(cell);
+    } else if (currentMode === 'erase') {
+        eraseBg(cell);
     }
-    console.log(squares);
+}
+let input = document.querySelector('#gridRange');
+let squaresValue = input.value;
+let gridResult = document.querySelector('#gridResult');
+gridResult.textContent = '20x20';
+input.addEventListener('change', () => {
+    gridResult.textContent = `${squaresValue}x${squaresValue}`;
+    console.log(squaresValue);
 })
+
+applyBtn.addEventListener('click', () => {
+    if (squaresValue < 100) {
+        makeRows(squaresValue,squaresValue);
+    } 
+    })
